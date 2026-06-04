26ffb402bf20011384d4c342ab5bc799d4505d7d
Henry <77836346+HenrysHouses@users.noreply.github.com>
2026-06-04 19:16:35 +0200

    main test

--- src/main.c
+++ src/main.c
@@ -1,16 +1,17 @@
+#include <stdio.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
-    // // returns zero on success else non-zero
-    // if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-    //     printf("error initializing SDL: %s\n", SDL_GetError());
-    // }
-    // SDL_Window* win = SDL_CreateWindow("GAME",
-    //                                    SDL_WINDOWPOS_CENTERED,
-    //                                    SDL_WINDOWPOS_CENTERED,
-    //                                    1000, 1000, 0);
-    // while (1)
-    //     ;
-    //
-    return 0;
+  if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS))
+  {
+
+    return -1;
+  }
+
+  printf("hello world!\n");
+  SDL_Quit();
+
+  return 0;
 }
